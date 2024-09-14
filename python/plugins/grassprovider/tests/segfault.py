70fa2af1190d29a6df5ff0d1fbddad9365a1c5cd
Thomas Wessel <mrvelle@gmail.com>
2024-09-14 13:41:37 +0200

    segfault.py, added comment about start_app(False)

--- python/plugins/grassprovider/tests/segfault.py
+++ python/plugins/grassprovider/tests/segfault.py
@@ -2,8 +2,8 @@ from osgeo import gdal
 from osgeo.gdalconst import GA_ReadOnly
 from qgis.testing import start_app
 
-start_app()
 dataset = gdal.Open('/home/velle/a/QGIS/tests/testdata/raster/rgb_with_mask.tif', GA_ReadOnly)
+start_app()
 print('the end')
 
 '''
@@ -15,10 +15,16 @@ If any of them are commented out, the segfault disappears.
 
 I tried with a few different tif, and the result is the same. 
 
-If changing the order of the two lines, some times it results in segfault, and sometimes it results in the following fatal error:
+If changing the order of the two lines, some times it results in segfault, and
+sometimes it results in the following fatal error:
 
-    double free or corruption (!prev)
-    Aborted
+    double free or corruption (!prev) Aborted
 
 The print line is always executed before the fatal error occurs. 
+
+If I modify to:
+
+    start_app(False)
+
+that will prevent it from calling exitQGIS(), and then there is no error. 
 '''
