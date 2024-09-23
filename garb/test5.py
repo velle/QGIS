ed5238f7904aed852385729d77d5a20950a26fd4
Thomas Wessel <mrvelle@gmail.com>
2024-09-23 09:29:06 +0200

    test5.py, added comment

--- garb/test5.py
+++ garb/test5.py
@@ -3,7 +3,10 @@ from qgis.core import QgsApplication
 def get_app():
     print('setup')
     app = QgsApplication([], False)
-    QgsApplication.instance()
+
+    # it makes no difference, whether I call this or not
+    # app = QgsApplication.instance()
+
     return app
 
 app = get_app()
